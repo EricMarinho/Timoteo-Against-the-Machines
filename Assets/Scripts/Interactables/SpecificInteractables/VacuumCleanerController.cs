0d8582c01b481241158648668e5026e7b4e31e1b
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-09-02 14:09:21 -0300

    Add Vacuum Behaviour

--- Assets/Scripts/Interactables/SpecificInteractables/VacuumCleanerController.cs
+++ Assets/Scripts/Interactables/SpecificInteractables/VacuumCleanerController.cs
@@ -1,3 +1,4 @@
+using Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,22 @@ namespace Objects
 {
     public class VacuumCleanerController : MonoBehaviour
     {
+        private InteractableHandler interactableHandler;
+
         [SerializeField] private float turnTime;
         [SerializeField] private float moveSpeed;
         private float turnTimer = 0f;
         private float turnDirection = 1f;
 
+        private void Start()
+        {
+            interactableHandler = GetComponent<InteractableHandler>();
+        }
+
         void Update()
         {
+            if (!interactableHandler.isActive) return;
+
             turnTimer += Time.deltaTime;
             transform.position += new Vector3(moveSpeed * Time.deltaTime * turnDirection, 0f, 0f);
 
